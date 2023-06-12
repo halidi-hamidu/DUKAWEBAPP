1515c05b8dc00689ef7b7d31b1c2aa11c13461a3
alfani <alfanishekw@gmail.com>
2023-06-12 10:01:43 +0300

    second commit

--- duka/settings.py
+++ duka/settings.py
@@ -118,13 +118,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_ROOT = BASE_DIR/'media' # plae where we gone store our images
-MEDIA_URL = '/media/' # place where we gona acces  the images file
 
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-os.path.join(BASE_DIR, 'static'),
+   os.path.join(BASE_DIR, 'static'),
 )
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
+
 
 LOGIN_URL ='loginPage'
 LOGIN_REDIRECT_URL ='homePage'
